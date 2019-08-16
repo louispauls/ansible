6da4132826a8443cc6ee8272fb42045311c47f65
louispauls <hlpauls@gmail.com>
2019-08-16 14:06:26 -0400

    Update preconfig.sh

--- lod/preconfig.sh
+++ lod/preconfig.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-echo Installing Ansible
-yum -y install ansible
-pip install netapp-lib
+echo ----------->>>       Installing Ansible           <<<------------------
+yum -y install ansible > /dev/null
+pip -q install netapp-lib > /dev/null
 
-echo Configuring the ansible hosts file
+echo ----------->>> Configuring the ansible hosts file <<<------------------
 cp -p /etc/ansible/hosts /etc/ansible/hosts.bak
-mv ./hosts /etc/ansible/hosts
+mv ./hosts /etc/ansible/hosts > /dev/null
