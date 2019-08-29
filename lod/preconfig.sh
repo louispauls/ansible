44ad1789fcddb13044cdd7de2c46ddbb76b69dc4
louispauls <hlpauls@gmail.com>
2019-08-29 15:30:52 -0400

    Delete preconfig.sh

--- lod/preconfig.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-echo ----------- Installing Ansible ------------------
-yum -y install ansible    > /dev/null
-pip -q install netapp-lib > /dev/null
-
-echo ----------- Configuring Ansible Hosts ------------------
-cp -p /etc/ansible/hosts /etc/ansible/hosts.bak
-mv ./hosts /etc/ansible/hosts > /dev/null
